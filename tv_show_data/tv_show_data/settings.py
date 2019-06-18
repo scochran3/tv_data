5c390066fbf6a55490d1828b4fd049f8e7f7d9e3
Shawn Cochran <scochran3@gmail.com>
2019-06-18 19:21:53 -0400

    Forgot to turn off Debug

--- tv_show_data/tv_show_data/settings.py
+++ tv_show_data/tv_show_data/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*h&++kz1v(-wu3o#30yo!f10ip&n)br2as=cq+z3x%xvyl@%##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
